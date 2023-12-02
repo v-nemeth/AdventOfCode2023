0ca9ff5210c4abf5ed836b70cdf3e128402352c3
nemeth <viktor@nemeth.dk>
2023-12-02 13:38:32 +0800

    Day 2 part one

---
+++ day_2/part2.rb
@@ -0,0 +1,16 @@
+def get_max_color(color, line)
+  max = 0
+  0..100.times do |amount|
+    max = amount if line.include?("#{amount} #{color}") and amount > max
+  end
+  max
+end
+
+lines = File.open('day_2/data.txt').readlines.map(&:chomp)
+
+sum = 0
+lines.each do |line|
+  sum += get_max_color('red', line) * get_max_color('green', line) * get_max_color('blue', line)
+end
+puts sum
+
