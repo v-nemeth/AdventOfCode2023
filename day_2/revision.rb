256fd7457d02751545f248ed089ddf8ecd3d2263
nemeth <viktor@nemeth.dk>
2023-12-02 22:02:03 +0800

    oneliner på del 2

--- day_2/revision.rb
+++ day_2/revision.rb
@@ -1,15 +1,14 @@
-lines = File.open('day_2/data.txt').readlines.map(&:chomp)
-
-
-
-lines.each do |line|
-  puts line.gsub(' blue','B').gsub(' red','R').gsub(' green','G')
-  puts line.gsub(' blue','B').gsub(' red','R').gsub(' green','G').scan(/(\d{1,2})R|(\d{1,2})G|(\d{1,2})B/).to_s
-
-  puts line.gsub(' blue','B').gsub(' red','R').gsub(' green','G').scan(/(\d{1,2})R|(\d{1,2})G|(\d{1,2})B/).transpose.map(&:compact).to_s
-  puts line.gsub(' blue','B').gsub(' red','R').gsub(' green','G').scan(/(\d{1,2})R|(\d{1,2})G|(\d{1,2})B/).transpose.map(&:compact).map {|sub| sub.map(&:to_i).max}.to_s
-  puts line.gsub(' blue','B').gsub(' red','R').gsub(' green','G').scan(/(\d{1,2})R|(\d{1,2})G|(\d{1,2})B/).transpose.map(&:compact).map {|sub| sub.map(&:to_i).max}.inject(:*)
-end
 
 # Heres the oneliner
 puts File.open('day_2/data.txt').readlines.map(&:chomp).map{ |line| line.gsub(' blue','B').gsub(' red','R').gsub(' green','G').scan(/(\d{1,2})R|(\d{1,2})G|(\d{1,2})B/).transpose.map(&:compact).map {|sub| sub.map(&:to_i).max}.inject(:*)}.sum
+
+# And the oneliner without gsub formatted to be more readable
+puts File.open('day_2/data.txt').readlines.map(&:chomp)
+         .map{ |line|
+           line.scan(/(\d{1,2}) red|(\d{1,2}) green|(\d{1,2}) blue/)
+               .transpose
+               .map(&:compact)
+               .map {|sub|
+                 sub.map(&:to_i).max
+               }.inject(:*)
+         }.sum
